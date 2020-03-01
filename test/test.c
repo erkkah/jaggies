d95fba9b235f02b5a5f0fb36feadd31bfd9579ee
Erik Agsjö <erik.agsjo@gmail.com>
2020-03-01 14:18:44 +0100

    Render test to file

--- test/test.c
+++ test/test.c
@@ -1,6 +1,8 @@
 #include "tigr.h"
+#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
 #include "jaggies.h"
 
@@ -9,7 +11,7 @@ TPixel white = {
 };
 
 TPixel gray = {
-    0x22, 0x22, 0x22, 0xff
+    0x11, 0x11, 0x11, 0xff
 };
 
 typedef struct JContext {
@@ -37,27 +39,34 @@ void animate(Tigr* screen, float time) {
 
     const int outerEdges = 5;
     const int innerEdges = 4;
-    jaggiePoint points[outerEdges + 1 + innerEdges + 1];
+    jaggiePoint points[1 + outerEdges + 1 + innerEdges];
     jaggiePoint* p = points;
+    jaggieClear();
 
     {
         float edgePhase = pos1;
         float edgeInc = (2 * M_PI) / outerEdges;
+
+        p->x = outerEdges;
+        p->y = 1;
+        p++;
+
         for(int i = 0; i < outerEdges; i++, p++){
             float x = cosf(edgePhase);
             float y = sinf(edgePhase);
-            p->x = x * 100 + 100;
-            p->y = y * 100 + 100;
+            p->x = x * 90 + 100;
+            p->y = y * 90 + 100;
             edgePhase += edgeInc;
         }
-        p->x = -2;
-        p->y = -2;
-        p++;
     }
 
     {
         float edgePhase = pos2;
         float edgeInc = (2 * M_PI) / innerEdges;
+        p->x = innerEdges;
+        p->y = 0;
+        p++;
+
         for(int i = 0; i < innerEdges; i++, p++){
             float x = cosf(edgePhase);
             float y = sinf(edgePhase);
@@ -65,19 +74,16 @@ void animate(Tigr* screen, float time) {
             p->y = y * 95 + 100;
             edgePhase += edgeInc;
         }
-        p->x = -1;
-        p->y = -1;
     }
 
-    jaggieClear();
     jaggiePoly(points);
 
     jaggiePoint bar[] = {
+        {4, 0},
         {5, 95},
         {195, 95},
         {195, 105},
         {5, 105},
-        {-1, -1}
     };
     jaggiePoly(bar);
 
@@ -96,7 +102,35 @@ void animate(Tigr* screen, float time) {
     jaggieLine(5, 16, 0, 11);
 }
 
-int main() {
+static void renderImages(const char* prefix) {
+    int len = strlen(prefix);
+    char* buf = (char*) malloc(len + 10);
+
+    Tigr* bmp = tigrBitmap(200, 200);
+    float time = 0;
+    float totalTime = 12;
+    float step = 1.0/15.0;
+    int pic = 0;
+    do {
+        time += step;
+
+        tigrClear(bmp, tigrRGB(0, 0, 0));
+        animate(bmp, step);
+        JContext jc = {
+            bmp->pix
+        };
+        jaggieRender(200, 200, setPixel, &jc);
+        sprintf(buf, "%s_%0.3d.png", prefix, pic);
+        tigrSaveImage(buf, bmp);
+
+        pic++;
+    } while (time < totalTime);
+
+    free(buf);
+    tigrFree(bmp);
+}
+
+static void renderScreen() {
     Tigr *screen = tigrWindow(200, 200, "jaggies", TIGR_FIXED);
 
     int pause = 0;
@@ -104,16 +138,16 @@ int main() {
 
     while (!tigrClosed(screen) && !tigrKeyDown(screen, TK_ESCAPE))
     {
-        float now = tigrTime();
+        float time = tigrTime();
         int key = tigrReadChar(screen);
         if(key == ' ') {
             pause ^= 1;
         } else if (key == 'm') {
             markers ^= 1;
         }
-        tigrClear(screen, tigrRGB(0x00, 0x00, 0x00));
+        tigrClear(screen, tigrRGB(0, 0, 0));
         if(pause == 0) {
-            animate(screen, now);
+            animate(screen, time);
         }
 
         JContext jc = {
@@ -131,5 +165,17 @@ int main() {
         tigrUpdate(screen);
     }
     tigrFree(screen);
+}
+
+int main(int argc, char** argv) {
+    if (argc == 2) {
+        const char* savePrefix = argv[1];
+
+        renderImages(savePrefix);
+    } else {
+        renderScreen();
+    }
+
     return 0;
 }
+
